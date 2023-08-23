fd6ccceafc3d226ed14f4290046d318fcdb933a1
leolucena22 <leonardolucena44@gmail.com>
2023-08-23 20:14:07 -0300

    Aula 6 - desafio concluido ✅

---
+++ Curso/Aula 06/Desafio/01.rb
@@ -0,0 +1,29 @@
+class Esportista 
+  def competir
+    puts "Participando de uma competição"
+  end
+end
+
+class JogadorDeFutebol < Esportista
+  def correr
+    puts "Correndo atrás da bola"
+  end
+end
+
+class Maratonista < Esportista
+  def correr
+    puts "Percorrendo o circuito"
+  end
+end
+
+jogador = JogadorDeFutebol.new
+
+puts "Jogador\t"
+jogador.competir
+jogador.correr
+
+puts "\nMaratonista"
+maratonista = Maratonista.new
+maratonista.competir
+maratonista.correr
+
